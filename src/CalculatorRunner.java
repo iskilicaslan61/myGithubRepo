3fc9f77aaa0ae7f242cee308aa1d908e548e1b88
ismailkilicaslan61@gmail.com <iskilicaslan61@gmail.com>
2023-06-29 14:40:34 +0200

    last code

--- src/CalculatorRunner.java
+++ src/CalculatorRunner.java
@@ -7,6 +7,7 @@ public class CalculatorRunner {
         calculator.add(4, 6);
         calculator.subtract(4, 2);
         System.out.println(calculator.multiply(10,5));
+        System.out.println("This is the last code...");
 
 
 
